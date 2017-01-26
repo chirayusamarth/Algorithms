ec764629603c17c39f793a4e011933fad16f2250
chirayusamarth <chirayu8samarth@yahoo.in>
2017-01-25 17:21:06 -0800

    Arrays 2D
    Given a  2D Array, A:
    
    1 1 1 0 0 0
    0 1 0 0 0 0
    1 1 1 0 0 0
    0 0 0 0 0 0
    0 0 0 0 0 0
    0 0 0 0 0 0
    We define an hourglass in A to be a subset of values with indices falling in this pattern in 's graphical representation:
    
    a b c
      d
    e f g
    
    There are  hourglasses in A, and an hourglass sum is the sum of an hourglass' values.
    
    Task 
    Calculate the hourglass sum for every hourglass in A, then print the maximum hourglass sum.

---
+++ ArraysAndStrings/Arrays2D.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+
+public class Arrays2D {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int a[][]=new int[6][6];
+		for(int i=0;i<6;i++){
+			for(int j=0;j<6;j++){
+				a[i][j]=scan.nextInt();
+			}
+		}
+		scan.close();
+		int sum[][]=new int[4][4];
+		for(int i=0;i<4;i++){
+			for(int j=0;j<4;j++){
+				sum[i][j]=0;
+				for(int k=j;k<j+3;k++){
+					sum[i][j] += a[i][k]+a[i+2][k];
+				}
+				sum[i][j]+=a[i+1][j+1];
+			}
+		}
+		for(int i=0;i<4;i++){
+			for(int j=0;j<4;j++){
+				System.out.print(" "+sum[i][j]);
+			}
+			System.out.println();
+		}
+		
+		int max = sum[0][0];
+		for(int i=0;i<4;i++){
+			for(int j=0;j<4;j++){
+				if(sum[i][j] > max)
+					max = sum[i][j];
+			}
+		}
+		System.out.println("Max: "+max);
+		
+	}
+
+}
